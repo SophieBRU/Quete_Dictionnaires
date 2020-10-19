de94e027ffc4ca89cd5eeb0996d2f62a7b5f1595
Sophie_BRULTET <sophie.brultet@gmail.com>
2020-10-19 13:43:03 +0200

    quête sur le dictionnaire

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Quete_Dictionnaire
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<char, string> frenchDictionary = new Dictionary<char, string>();
+            frenchDictionary['a'] = "abeille";
+            frenchDictionary['b'] = "brebis";
+            frenchDictionary['c'] = "cadeau";
+
+            Console.WriteLine("Dictionnaire :");
+
+            // Parcourir le dictionnaire
+            foreach (KeyValuePair<char, string> letter in frenchDictionary)
+            {
+                Console.WriteLine("Lettre : {0}, Valeur : {1}",
+                    letter.Key, letter.Value);
+            }
+
+            //suppression de la lettre C
+            frenchDictionary.Remove('c');
+
+
+            Console.WriteLine("Dictionnaire 2 :");
+
+            // Parcourir le dictionnaire
+            foreach (KeyValuePair<char, string> letter in frenchDictionary)
+            {
+                Console.WriteLine("Lettre : {0}, Valeur : {1}",
+                    letter.Key, letter.Value);
+            }
+        }
+    }
+}
